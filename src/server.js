57e1eee839e2af1e4086ab2a65b8c0914781ef9a
LiaMeyy <majusaavedracarvajal@gmail.com>
2026-08-11 22:32:23 -0500

    script:start

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const app = require('./app');
 const { env } = require('./config/env');
+const { connectDB } = require('./config/db');
 
-app.listen(env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${env.PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.listen(env.PORT, () => {
+      console.log(`Servidor corriendo en puerto ${env.PORT}`);
+    });
+  } catch (error) {
+    console.error('No se pudo iniciar el servidor:', error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
